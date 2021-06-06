3a2cb7f45e3b97095cd4b98244059de0faad1772
KarmaZA <EVRJON003@myuct.ac.za>
2021-06-06 23:09:01 +0200

    Tired finish tomorrow

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.net.Socket;
 
+
 public class Client{
 	public static void main(String[] args){
 		Socket socket = null;
--- src/server/ChatServer.java
+++ src/server/ChatServer.java
@@ -1,38 +1,44 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 
+import java.util.List;
 
+//https://gist.github.com/fliedonion/1002293af6fd043fbd6e729c13018562
 
 public class ChatServer{
 
-	private static ServerSocket serverSocket;
+	private ServerSocket serverSocket;
 	private static int portNumber = 4444;
+	private int serverPort;
+	private List<ClientThread> clients;
 
-		public static void main(String[] args){
-		portNumber = 4444; //If this doesn't work set up a different port (don't go lower than 1000 most likely in use already)
+	public static void main(String[] args) {
+		ChatServer server = new ChatServer(portNumber);
+		server.startServer();
+	}
+
+	public ChatServer(int PN){ this.serverPort =PN; }
+
+	private void startServer(){
 		serverSocket = null;
 		/*
 		* Setting up a listener on a specific port in a try catch block in case it fails.
 		*/
 		try {
 			serverSocket = new ServerSocket(portNumber);
-			acceptClients(); //This will need to be changed to only deal with Alice and Bob
+			acceptClients(serverSocket); //This will need to be changed to only deal with Alice and Bob
 		} catch (IOException e){
 			System.err.println("Could not set up listener on: " + portNumber);
 			System.exit(1);
 		}
 	}
 
-	public static void acceptClients(){
-		ArrayList clients = new ArrayList<ClientThread>(); //ArrayList for storing clients
-
+	public void acceptClients(ServerSocket serverSocket){
 	/*
 	* So this is going to be in parallel but for the actual project
 	* we only need two agents so it's unlikely threads will be necessary
 	*/
-
 		while(true){
 			try{		//infinite loop
 				Socket socket = serverSocket.accept();
